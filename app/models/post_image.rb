6debf8d3ba52d8fcd163282ae995ae22186dbe1a
EC2 Default User <ec2-user@ip-172-31-1-67.ap-northeast-1.compute.internal>
2022-10-12 08:23:36 +0000

    [Finish] 5-24

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -16,9 +16,9 @@ class PostImage < ApplicationRecord
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      'no_image.jpg'
+      image
   end
   
 end
